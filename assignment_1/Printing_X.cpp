865c18769b5e85e59e0e7e1b44a714ffad998bd9
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-06-29 17:43:23 +0600

    added extra code for priting x pattern

--- assignment_1/Printing_X.cpp
+++ assignment_1/Printing_X.cpp
@@ -5,5 +5,28 @@ int main() {
     int n;
     cin >> n;
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j && i + j != (n - 1))
+            {
+                cout << "\\";
+            }
+            else if (i == j && i + j == (n - 1))
+            {
+                cout << "X";
+            }
+            else if (i + j == (n - 1) && i != j)
+            {
+                cout << "/";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        
+        cout << endl;
+    }
 }
